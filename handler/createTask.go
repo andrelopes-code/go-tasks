6eeaa5d2c44f5e805653a3fae226f1d61fe00fd1
André Lopes <andrelopes.code@gmail.com>
2024-05-18 05:40:02 +0000

    ✨ feat: add createTask handler logic

--- handler/createTask.go
+++ handler/createTask.go
@@ -3,9 +3,34 @@ package handler
 import (
 	"net/http"
 
+	"github.com/andrelopes-code/go-tasks/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateTaskHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Create a Task"})
+	request := CreateTaskRequest{}
+	ctx.BindJSON(&request)
+
+	// Validate the request data
+	if err := request.validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Create a new task on database
+	task := schemas.Task{
+		Title:       request.Title,
+		Description: request.Description,
+		Done:        false,
+	}
+
+	// Try to create the task on database
+	if err := db.Create(&task).Error; err != nil {
+		logger.Errorf("error creating task: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating task on database")
+		return
+	}
+
+	sendSuccess(ctx, "create-task", task)
 }
--- router/router.go
+++ router/router.go
@@ -1,12 +1,15 @@
 package router
 
 import (
+	"github.com/andrelopes-code/go-tasks/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func Initialize() {
 	// initialize router
 	router := gin.Default()
+	// initialize handler
+	handler.InitializeHandler()
 	// initialize routes
 	initializeRoutes(router)
 	// start server
