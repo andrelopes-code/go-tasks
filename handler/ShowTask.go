ccacacf502e3607e663567ccfd3114accf39775a
André Lopes <andrelopes.code@gmail.com>
2024-05-18 10:08:01 +0000

    ✨ feat: add showTask handler logic

--- handler/ShowTask.go
+++ handler/ShowTask.go
@@ -1,11 +1,31 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/andrelopes-code/go-tasks/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowTaskHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Get a Task"})
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"detail": "invalid id, id must be a number",
+			"code":   http.StatusBadRequest,
+		})
+		return
+	}
+
+	task := schemas.Task{}
+
+	// Check if the task with the ID exists
+	if err := db.First(&task, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("task with id: %d not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "show-task", task)
 }
