2a3d66888fdfb79b4d9c4ce6670cdd7ca156e567
André Lopes <andrelopes.code@gmail.com>
2024-05-18 01:51:25 +0000

    🏷️ feat: add task response struct

--- schemas/task.go
+++ schemas/task.go
@@ -1,12 +1,24 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Task struct {
 	gorm.Model
-	Title string
+	Title 		string
 	Description string
-	Done bool
+	Done 		bool
+}
+
+type TaskResponse struct {
+	ID 			uint 		`json:"id"`
+	CreatedAt 	time.Time 	`json:"createdAt"`
+	UpdatadAt 	time.Time 	`json:"updatedAt"`
+	DeletedAt 	time.Time 	`json:"deletedAt,omitempty"`
+	Title 		string 		`json:"title"`
+	Description string 		`json:"description"`
+	Done 		bool 		`json:"done"`
 }
